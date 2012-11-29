0c439a9418512ed54102dc9c65278a2096e155e6
Mike Matrix <m.braun92@gmail.com>
2012-11-29 17:54:03 +0100

    * Added SniperPerformerBuilder stub

--- src/main/java/com/thevoxelbox/voxelsniper/SniperPerformerBuilder.java
+++ src/main/java/com/thevoxelbox/voxelsniper/SniperPerformerBuilder.java
@@ -1,6 +1,11 @@
 package com.thevoxelbox.voxelsniper;
 
+import com.thevoxelbox.voxelgunsmith.Performer;
+
 public class SniperPerformerBuilder
 {
-
+	public Performer buildPerformer(char target, char filter, String options)
+	{
+		return null;
+	}
 }
